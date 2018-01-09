97f67027d4ba10299f2543f02fc3651456fef4a6
Matej Janjic <matej.janjic@infinum.hr>
2018-01-09 18:49:23 +0100

    Setup JSS

--- client/components/Whiteboard.js
+++ client/components/Whiteboard.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import injectSheet from 'react-jss';
+
+const styles = {
+  wrapper: {
+    backgroundColor: 'yellow',
+    height: '200px'
+  },
+  label: {
+    fontWeight: 'bold'
+  }
+};
 
 class Whiteboard extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
+      <div className={classes.wrapper}>
         <canvas></canvas>
       </div>
     );
   }
 }
 
-export default Whiteboard;
+Whiteboard.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default injectSheet(styles)(Whiteboard);
--- client/pages/App.js
+++ client/pages/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 
 import Whiteboard from '../components/Whiteboard';
 
+import SocketService from '../services/websocket';
+
 class App extends Component {
+  componentWillMount() {
+    this.drawingService = new SocketService('drawing');
+  }
+
   render() {
     return (
       <div>
-        <Whiteboard/>
+        <Whiteboard />
       </div>
     );
   }
